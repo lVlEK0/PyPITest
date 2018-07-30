5efb7554f00c54915dfe4f434eb4a0bdc04efee7
KASE <kaseyutaka.suffix@gmail.com>
2018-07-30 21:33:11 +0900

    delete happy

--- setup.py
+++ setup.py
@@ -4,11 +4,10 @@ setup(name="pypitest",
 	version="0.1.0",
 	description="Test for PyPI",
 	url="https://github.com/IVIEK0/PyPITest.git",
-	packages=['test', 'test.foo'],
+	packages=['pypitest'],
 	install_requires=['setuptools',],
 	entry_points={
 		'console_scripts': [
-			'happy = test.foo.hoge:main',
 			'sad = test.fuga:main'
 		]
 	}
