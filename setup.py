4d476f32f3caa67379f1d762b33d8a7bd91448e5
KASE <kaseyutaka.suffix@gmail.com>
2018-07-30 20:16:02 +0900

    first commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(name="pypitest",
+	version="0.1.0",
+	description="Test for PyPI",
+	url="https://github.com/IVIEK0/pypi.git",
+	packages=find_packages(),
+	entry_points="""
+	[console_scripts]
+	happy = test.foo.hoge:main
+	""",
+	)
+
---
+++ test/foo/hoge.py
@@ -0,0 +1,6 @@
+def main():
+	print("I am so happy")
+
+if __name__ = '__main__':
+	main()
+
