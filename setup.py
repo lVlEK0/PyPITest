01c522de80b678d17927a6cf8cb9efd89550d234
KASE <kaseyutaka.suffix@gmail.com>
2018-07-31 03:10:09 +0900

    clear

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-	packages=["pypitest", "pypitest.foo"],
-	entry_points={
+	name = 'PyPITest',
+    version = '1.0.0',
+    url = 'https://github.com/lVlEK0/PyPITest.git',
+    license = 'Kusoge',
+    author = 'Yutaka Kase',
+    author_email = 'kaseyutaka.suffix@gmail.com',
+    description = 'test for pip install git+',
+    install_requires = ['setuptools'],
+	packages = ["pypitest", "pypitest.foo"],
+	entry_points = {
 		'console_scripts': [
 			'sad = pypitest.sad:main',
 			'hoge = pypitest.foo.hoge:main'
