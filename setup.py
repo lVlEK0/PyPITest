fe2b8222d08b71252261b061d67b018444fa68f1
KASE <kaseyutaka.suffix@gmail.com>
2018-07-31 02:08:35 +0900

    modified entry_points

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
 	packages=["pypitest", "pypitest.foo"],
-	entry_points="""
-	[console_scripts]
-	sad = pypitest.sad:main
-	hoge = pypitest.foo.hoge:main
-	""",
+	entry_points={
+		'console_scripts': [
+			'sad = pypitest.sad:main'
+			'hoge = pypitest.foo.hoge:main'
+		]
+	}
 )
