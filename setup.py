fa77d84e93d46131a0007a1f3fca20077ba8bf93
KASE <kaseyutaka.suffix@gmail.com>
2018-07-31 02:02:58 +0900

    modified packages

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
 	name="PyPITest",
 	version="0.1.0",
-	packages=["pypitest"],
+	packages=["pypitest", "pypitest.foo"],
 	url="https://github.com/IVIEK0/PyPITest.git",
 	description="Test for PyPI",
 	install_requires=['setuptools'],
