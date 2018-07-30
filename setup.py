7978bd3d5eee793149a902e76296f46639fcfba7
KASE <kaseyutaka.suffix@gmail.com>
2018-07-30 22:32:59 +0900

    cf. pig

--- setup.py
+++ setup.py
@@ -1,15 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
 	name="test",
 	version="0.1.0",
-	packages=['test'],
+	packages=find_packages(),
 	url="https://github.com/IVIEK0/PyPITest.git",
 	description="Test for PyPI",
 	install_requires=['setuptools'],
-	entry_points={
-		'console_scripts': [
-			'sad = test.test:main'
-		]
-	}
+	entry_points="""
+	[console_scripts]
+	sad = test.test:main
+	""",
 )
