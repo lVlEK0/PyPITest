7b311663408ea2eaf9cb38477cc9254872e4b92d
KASE <kaseyutaka.suffix@gmail.com>
2018-07-31 02:01:09 +0900

    rename

--- setup.py
+++ setup.py
@@ -3,14 +3,13 @@ from setuptools import setup, find_packages
 setup(
 	name="PyPITest",
 	version="0.1.0",
-	packages=["pypi"],
+	packages=["pypitest"],
 	url="https://github.com/IVIEK0/PyPITest.git",
 	description="Test for PyPI",
 	install_requires=['setuptools'],
 	entry_points="""
 	[console_scripts]
-	sad = pypi.sad:main
-	hoge = pypi.foo.hoge:main
-	testp = testp:main
+	sad = pypitest.sad:main
+	hoge = pypitest.foo.hoge:main
 	""",
 )
--- testp.py
+++
@@ -1,7 +0,0 @@
-from test import test
-
-def main():
-    test.main()
-
-if __name__ == '__main__':
-    main()
