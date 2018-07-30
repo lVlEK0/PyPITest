ea6f24c1b76db30360f3e27e9945b0df7e818be9
KASE <kaseyutaka.suffix@gmail.com>
2018-07-30 21:19:19 +0900

    cf. wasm

--- setup.py
+++ setup.py
@@ -4,11 +4,13 @@ setup(name="pypitest",
 	version="0.1.0",
 	description="Test for PyPI",
 	url="https://github.com/IVIEK0/PyPITest.git",
-	packages=find_packages(),
-	entry_points="""
-	[console_scripts]
-	happy = test.foo.hoge:main
-	sad = test.fuga:main
-	""",
-	)
+	packages=['test', 'test.foo'],
+	install_requires=['setuptools',],
+	entry_points={
+		'console_scripts': [
+			'happy = test.foo.hoge:main'
+			'sad = test.fuga:main'
+		]
+	}
+)
 
